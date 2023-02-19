cbe5f025357c4ef5e19d353dfcb82316aa500a3a
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-19 14:57:58 -0800

    fixed main description

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ *  * main - Print message using puts
+ *  * Return: 0
+ *  */
+
 int main(void)
 
 {
-	char tmp[] = "Programming is like building a multilingual puzzle";
+	char tmp[] = ("\"Programming is like building a multilingual puzzle");
 
 	puts(tmp);
 
