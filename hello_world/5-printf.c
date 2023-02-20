248e6c18229ec198d38805d88b8cfb4671bcfea9
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-20 00:31:03 -0800

    bruh

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/*
-	 main - Print message with printf
-	 Return: 0
-*/
-
+/**
+ *  * main - Prints quote
+ *   *
+ *    * Return: 0
+ *     *
+ *      */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
