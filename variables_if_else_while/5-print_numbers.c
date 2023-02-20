838c7f1e8d92db68bbf70a0203c57e145ca2c1fb
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-20 08:47:41 -0800

    works greeat

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints 0-9
+* printsb base 10 numbers
+* Return:(0)
+*/
+
+int main(void)
+{
+	int chiffre = 0;
+
+	for (; chiffre < 10; chiffre++)
+		{
+		printf("%d", chiffre);
+		}
+putchar('\n');
+return (0);
+}
