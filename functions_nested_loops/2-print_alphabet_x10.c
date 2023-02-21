c156f531eea5af1c487dbac4b7c76c1fa29484b7
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-21 07:42:13 -0800

    prints alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet x10
+ * Return: 0
+*/
+void print_alphabet_x10(void)
+
+{
+int x = 0;
+
+	while (x < 11)
+	{
+		int i = 97;
+
+		for (; i <= 122; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+	x++;
+	}
+}
