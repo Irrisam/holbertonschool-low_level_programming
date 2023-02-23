b60e7ff1a5c648b5164d8f428bb4b06c8e17b20c
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-23 07:30:46 -0800

    10 simple number summer

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - prints sum of numberz
+ * @a: first number to be summed
+ * @b: second number to be summed
+ *
+ * Return: c
+ *
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+return (c);
+}
---
+++ functions_nested_loops/test10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int add(int i, int j)
+{
+
+	char str1[100];
+	char str2[100];
+
+for (i = 0; str1[i]!='\0'; i++);
+
+
+}
