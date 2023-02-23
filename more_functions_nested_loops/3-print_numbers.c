b59c7d2a06e541fbbf3ff271e4e19e25cf2296b0
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-23 08:20:37 -0800

    still very similar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * * print_numbers - Prints 0-9
+ * * printsb base 10 numbers
+ * * Return:(0)
+*/
+
+void print_numbers(void)
+{
+	int chiffre = 48;
+	int newline = 10;
+
+	for (; chiffre < 58; chiffre++)
+	{
+		putchar(chiffre);
+	}
+		putchar(newline);
+	return (0);
+	}
