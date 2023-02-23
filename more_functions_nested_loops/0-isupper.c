de44acecd03057eacd614002738764a2f73f2585
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-23 07:53:21 -0800

    checks for uppercase the same way as in earlier project

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * _isupper - checks for caps
+ *   * @c: input for function
+ *    * Return: 0
+ *    */
+
+int _isupper(int c)
+
+{
+	if (c > 64 && c < 91)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
