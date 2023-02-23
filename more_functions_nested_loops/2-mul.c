13b90a103b0a3e1c501a64c83e87155a1bb862cf
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-23 08:15:16 -0800

    still the same....

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * mul - prints multiple of numberz
+ *   * @a: first number to be multiplied
+ *    * @b: second number to be multiplied
+ *     *
+ *      * Return: c
+ *       *
+ *       */
+int mul(int a, int b)
+{
+		int c;
+
+			c = a * b;
+			return (c);
+}
