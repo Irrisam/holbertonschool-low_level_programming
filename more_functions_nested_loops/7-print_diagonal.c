2a5973325405528e3cc2cd07e1280eef0c0e1138
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-24 04:10:02 -0800

    it was hawqrd but it worked

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - prionts n lines
+ * @n: lenght of the line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j = 0;
+
+	while (n > 0)
+	{
+		i = 0;
+		while (i < j)
+		{
+			_putchar(32);
+			i++;
+		}
+		n--;
+		_putchar(92);
+		_putchar(10);
+		j++;
+	}
+}
