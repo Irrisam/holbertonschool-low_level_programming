8d0b1c38c28a618550aa81e3ffe38f7a124b216c
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-25 09:18:07 -0800

    finnally worked the bug out

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  * print_square - square of int size size
+ *   * @size: square sizes
+ *    */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		i = 0;
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar(35);
+				j++;
+			}
+			_putchar(10);
+			i++;
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
