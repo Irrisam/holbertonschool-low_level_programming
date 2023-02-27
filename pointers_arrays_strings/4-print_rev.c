e9e4a26372cc89d2691959020e1556b94a7066ed
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-27 07:58:01 -0800

    prints in reverse, works

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse prints
+ *@s: reversed string
+ *
+*/
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	while (a != 0)
+	{
+		_putchar(s[a]);
+		a--;
+	}
+	_putchar(10);
+}
+
