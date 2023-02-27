5bb363b710a822d378c6f66a9bd38febb017956f
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-27 05:48:51 -0800

    fixed betty once again

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strlen - counts the number of iterations
+ *@s: inputedf char
+ * Return: number of chars in string
+*/
+
 int _strlen(char *s)
 {
-	int mabite = 0;
+	int a = 0;
+
 	while (*s != '\0')
 	{
-		mabite++;
+		a++;
 		s++;
 	}
-	return (mabite);
+	return (a);
 }
