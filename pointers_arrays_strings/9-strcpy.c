0b444545173e2bc0649ffaeeb5e25ad581dd4cce
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-02-28 07:36:24 -0800

    works properly

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strcpy - tranfers char array from srx to dest
+ *@src: src array of chars
+ *@dest:dest array of chars
+ * Return: array of copied string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int compteur = 0;
+	int i;
+
+	while (src[compteur] != '\0')
+	{
+	compteur++;
+	}
+
+	for (i = 0 ; i < compteur; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
