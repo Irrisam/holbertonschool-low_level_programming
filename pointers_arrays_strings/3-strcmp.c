2d494155cc20268d1096f89aebcee23f8af4c714
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-02 02:35:20 -0800

    i dont really know why but chat gpt knew

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- *
- */
+*_strcmp - compares strings and returns difference
+* @s1: input comparing to
+* @s2 : input compared to
+* Return: difference beetween values and 0 if similar
+*/
 
 int _strcmp(char *s1, char *s2)
 {
 	int compteur = 0;
 
-	while(s1[compteur] && s2[compteur] == s1[compteur])
+	while (s1[compteur] && s2[compteur] == s1[compteur])
 	{
 		compteur++;
 	}
-		return(s1[compteur] - s2[compteur]);
+return (s1[compteur] - s2[compteur]);
 }
