96ece7cce9de59ff37915c7f1a1530ef7d6b313d
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-01 23:54:15 -0800

    once i got out of my stubborn sizeof idea, i can find the solution

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - appends src string to end of dest sring
+ * @dest: string to apprend
+ * @src: string to be appended to
+ * Return: finished string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int compteursrc = 0;
+	int compteurdest = 0;
+
+	while (dest[compteurdest] != '\0')
+	{
+		compteurdest++;
+	}
+	while (compteursrc < n && src[compteursrc] != '\0')
+	{
+		compteursrc++;
+		dest[compteurdest + compteursrc] = src[compteurdest];
+	}
+	dest[compteursrc + compteurdest] = '\0';
+	return (dest);
+}
