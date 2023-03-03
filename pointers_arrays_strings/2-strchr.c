0f7012e65c7e273a96649401dfa7ee847a882cb9
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-03 06:04:43 -0800

    okay fixed last if git add .

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int compteur = 0;
-
-	for (; *s != c ; compteur++)
+	for (; *s != '\0' ; s++)
 	{
-		if ((*s + compteur) == c)
+		if (*s  == c )
 		{
 			return (s);
 		}
+	}
+	if (*s == c)
+	{
+		return (0);
 	}
 	return (0);
 }
