847c3c7f856747d299bc90a92f26f6426119f485
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-03 13:50:05 -0800

    checkerfighter

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagsums - sorry again, i'll be back to solve this cute diagram
+ * @a: dont know
+ * @size: will know
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+
+	while (a[i] < size)
+	{
+		i++;
+	}
+}
