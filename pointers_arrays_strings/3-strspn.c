a5967e8f3dc5f3e48e05b5e7b82a4393aa312e03
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-03 07:05:50 -0800

    damn betty was tough

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- *
+ * _strspn - searches for prefixes in *s
+ *  @s:string to search in
+ *   @accept: prefix to look for
+ *   Return: Always 0.
  */
 
-unsigned int _strspn(char* s, char* accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int compteur = 0;
 	char *a;
-	
+
 	while (*s != '\0')
 	{
 		a = accept;
 
 		while (*a != '\0')
 		{
-			if (*s == *a) 
+			if (*s == *a)
 			{
 				compteur++;
 				break;
@@ -24,9 +27,9 @@ unsigned int _strspn(char* s, char* accept)
 		}
 		if (*a == '\0')
 		{
-			return compteur;
+			return (compteur);
 		}
 		s++;
 	}
-	return compteur;
+	return (compteur);
 }
