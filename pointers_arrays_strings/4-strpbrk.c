6dd7fa9314d28e12ec046754783b2ca822c9582a
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-03 07:49:33 -0800

    FINALLY

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,27 @@
 #include "main.h"
 /**
- * _strpbrk - searches for byte that are in accept 
+ * _strpbrk - searches for byte that are in accept
  * and return a pointer to a byte that matches it
  * @s:string searched in
  * @accept: string searched
  * Return: 0
  */
 
-char* _strpbrk(char* s, char* accept) 
+char *_strpbrk(char *s, char *accept)
 {
 	while (*s != '\0')
 	{
-		char* a = accept;
+		*a = accept;
+
 		while (*a != '\0')
 		{
 			if (*s == *a)
 			{
-				return s;
+				return (s);
 			}
 			a++;
 		}
 		s++;
 	}
-	return 0;
+	return (0);
 }
