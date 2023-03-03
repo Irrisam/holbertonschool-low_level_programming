24aadd7b119388cf5382a884a0af17df28c1ebe7
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-03 02:37:05 -0800

    my bad , fixed dest instead of src to copied on

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
  for(; compteur < n; compteur++)
  {
-	 *(src + compteur) = *(dest + compteur);
+	 *(dest + compteur) = *(src + compteur);
  }
  return (dest);
 }
