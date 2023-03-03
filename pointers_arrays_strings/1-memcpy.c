31432352611c8588b5c3145c3bef03292e613100
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-03 02:39:15 -0800

    my bad , fixed dest instead of src to copied on+betty

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,17 +4,18 @@
 /**
  *_memcpy - copies memory from src to dest
  *@src: src string to copy
- *@dest: dest string to be copied to 
+ *@dest: dest string to be copied to
+ *@n: given size of addresses
  * Return: dest pointer to copied file
-*/
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int compteur = 0;
 
- for(; compteur < n; compteur++)
- {
-	 *(dest + compteur) = *(src + compteur);
- }
- return (dest);
+	for (; compteur < n; compteur++)
+	{
+		*(dest + compteur) = *(src + compteur);
+	}
+	return (dest);
 }
