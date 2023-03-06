ee6633c3150fdbc21c42c45a30a74064de4e2283
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-06 08:14:35 -0800

    quite easy

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints out file name
+ * @argc: input of count
+ * @argv: vector
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
