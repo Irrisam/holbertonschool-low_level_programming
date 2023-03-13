ce1699b493467e4be7bed7557b7eae5c794aeacf
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-13 09:13:58 -0700

    all works fine

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ *  * malloc_checked - checkign if malloc is valid 
+ *   *@b: size of malloc to be made
+ *    * Return: pointer tot new string
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *array;
+
+	array = malloc(b);
+
+	if (array == NULL)
+	{
+		exit(98);
+	}
+	return (array);
+}
