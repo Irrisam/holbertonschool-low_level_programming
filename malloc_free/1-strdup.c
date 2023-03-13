271618537a4f2eb09293c78e919808cf5953a801
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-13 03:18:03 -0700

    should be working

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * _strdup - copies string into another string
+ * @str: string to be copied
+ *    * Return: copied string.
+ */
+
+char *_strdup(char *str)
+{
+	int compteur = 0;
+	int compteur2 = 0;
+	char *array;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	for (; str[compteur] != '\0'; compteur++)
+	{
+	}
+
+	array = malloc(compteur * sizeof(char));
+
+	for (; compteur2 < compteur; compteur2++)
+	{
+		array[compteur2] = str[compteur2];
+	}
+	return (array);
+}
