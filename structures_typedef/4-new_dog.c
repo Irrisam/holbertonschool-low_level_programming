0180753265d03849c0e95c4fefcb2e79296c9f35
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-20 06:49:48 -0700

    j'y crois pas frere

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t* bite = NULL;
+	int compteur = 0;
+	bite = malloc(sizeof(dog_t));
+
+	for (;compteur < name[compteur]; compteur++)
+	{
+	}
+	bite->name = malloc(compteur*sizeof(bite->name));
+	for (compteur = 0; compteur < owner[compteur]; compteur++)
+	{
+	}
+	bite->owner = malloc(compteur*sizeof(bite->owner));
+	if (bite == NULL || bite->name == NULL || bite->owner == NULL)
+	{
+		free(bite->name);
+		free(bite->owner);
+		free(bite);
+		return (NULL);
+	}
+	for (compteur = 0 ;name[compteur] != '\0'; compteur++)
+		bite->name[compteur] = name[compteur]; 
+	bite->name[compteur] = '\0';
+	bite->age = age;
+	for (compteur = 0 ;owner[compteur] != '\0'; compteur++)
+		bite->owner[compteur] = owner[compteur];
+	bite->owner[compteur] = '\0';
+	return (bite);
+}
