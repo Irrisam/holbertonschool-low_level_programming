a0f585217ee0706d394836398b975c796b2bd4f3
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-20 03:09:14 -0700

    okay then return with no value

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,15 +2,16 @@
 #include "dog.h"
 
 /**
- * init_dog - intializes variables
- * @d: type
- * @name: name
- * @age: age
- * @owner: owner
+ *
+ *
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
