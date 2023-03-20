9b4a0dda2d7ea9c975f9369cf4d93d060881a2ab
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-20 05:01:57 -0700

    still ez but betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,24 +2,20 @@
 #include "dog.h"
 
 /**
- *
+ *print_dog - struc
+ *@d: parameter
  */
 
 void print_dog(struct dog *d)
 
 {
-	
 	if (d->name == NULL)
 	{
-	d->name = "(nil)";
-	}
-	if (d->age == 0.0)
-	{
-	d->age = "(nil)";
+		d->name = "(nil)";
 	}
 	if (d->owner == NULL)
 	{
-		d->age = "(nil)";
+		d->owner = "(nil)";
 	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
