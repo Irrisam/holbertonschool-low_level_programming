97cc63e69fc1924bec0d09cdbaeb0f563caf5647
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-20 01:48:22 -0700

    done

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * main - defines doggo
+ * Return: NULL
+ */
+
+int main(void)
+{
+	struct dog
+	{
+		char * = name;
+		float = age;
+		char * =  owner;
+	}
+	return (0);
+}
+
+#endif
