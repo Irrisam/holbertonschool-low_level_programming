6fa224d0983af32b8369533c428854297977f87c
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-22 06:36:00 -0700

    okay mybad betty better

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -6,16 +6,16 @@
 
 
 /**
-* struct op - Struct op
+ * struct op - Struct op
  *
  *  * @op: The operator
  *   * @f: The function associated
- *    */
+*/
 
 typedef struct op
 {
-	    char *op;
-	        int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
