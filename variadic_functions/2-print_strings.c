1e1fffb24026eca60397cdfe88eabf5b7279b411
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-23 07:43:09 -0700

    seems done, working in local

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - print strings
+ * @separator: split strings
+ * @n: strings to look through
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *result = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (; i < n; i++)
+	{
+		result = va_arg(ap, char*);
+		printf("%s", result);
+
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
+
