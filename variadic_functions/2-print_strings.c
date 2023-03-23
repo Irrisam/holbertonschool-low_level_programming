0b2e9678f9de43768127a7a7990d793c9f5954b6
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-23 07:45:46 -0700

    seems done, working in local

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,8 +18,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (; i < n; i++)
 	{
 		result = va_arg(ap, char*);
-		printf("%s", result);
-
+		if (result == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", result);
+		}
 		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
