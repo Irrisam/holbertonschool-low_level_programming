03585199fb6eca9851981a17ad8ace51b7f0aa42
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-03-23 06:12:00 -0700

    betty for mainh

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,15 +1,13 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
 #include <stdlib.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
-/**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- */
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+#endif
