d659f13fbce48d4d55e666be286d70596acd0cff
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-04-03 09:11:08 -0700

    same as before but with no pritn ?

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns number of arguments
+ * @h: given linked list
+ * Return: count of arguments
+*/
+
+size_t list_len(const list_t *h)
+{
+
+	list_t *head = NULL;
+	int count = 0;
+
+	head = (list_t *) h;
+	while (head != NULL)
+	{
+			count++;
+			head = head->next;
+	}
+	return (count);
+}
