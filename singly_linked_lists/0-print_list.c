796f9e47914aa112bada2411cbdb92cad7ea1bd2
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-04-03 09:02:39 -0700

    works perfectly in local

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+
+	list_t * head = NULL;
+	int count = 0;
+
+	head = (list_t *) h;
+
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+			head = head->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", head->len, head->str);
+			count++;
+			head = head->next;
+		}
+	}
+	return(count);
+}
