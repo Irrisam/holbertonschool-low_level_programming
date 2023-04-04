1ec929ae73c412ec35d8ecaa86987bf3366ed118
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-04-04 08:29:11 -0700

    works thanks to temp

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,13 +12,31 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new = NULL;
+	list_t *new_node;
+	list_t *tmp;
 
-	new = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t) * 1);
 
-	new->str = strdup(str);
-	new->len = strlen(str);
-	new->next = *head;
-	*head = new;
-	return (*head);
+	if (!new_node)
+		return (NULL);
+
+	tmp = *head;
+
+	new_node->next = NULL;
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new_node;
+
+	return (new_node);
 }
