920e18b1559bd774142995287a3625f42b89c70d
Duchamp Tristan <tristanduchamp@hotmail.fr>
2023-04-06 07:53:33 -0700

    working in local +betty

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - indert a node a index pos in a list
+ * @h: pointer to list
+ * @idx: index of number to stop at
+ * @n: value of node
+ * Return: pointer to list
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int counter = 0;
+	dlistint_t *new = NULL;
+	dlistint_t *tmp = NULL;
+
+	tmp = *h;
+
+	if (*h == NULL)
+	{
+		return (*h);
+	}
+	while (counter != idx - 1)
+	{
+		if ((*h)->next == NULL)
+		{
+			return (*h);
+		}
+		*h = (*h)->next;
+		counter++;
+	}
+	new = malloc(sizeof(dlistint_t) * 1);
+
+	new->n = n;
+	new->prev = *h;
+	new->next = (*h)->next;
+	(*h)->next = new;
+	(*h)->next->prev = new;
+	(*h) = new;
+	*h = tmp;
+	return (*h);
+}
