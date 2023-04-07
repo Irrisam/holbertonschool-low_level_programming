5be45f9b4a80ae63bdd487f1e3d37342cce2fe44
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-07 12:22:57 +0200

    fixed out of range index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if ((*h)->next == NULL)
 		{
-			return (*h);
+			return (0);
 		}
 		*h = (*h)->next;
 		counter++;
