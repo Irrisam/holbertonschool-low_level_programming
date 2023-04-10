5e9eff58f3f2cea9ddd24bc1970560e5c21ecd1f
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-10 14:28:44 +0200

    fixed betty + bugs

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,30 +5,31 @@
  * @filename: name of the file to be read
  * @letters: number of letters to be printed
  * Return: count of printed chars
-*/
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE * fptr = NULL;
-    size_t count = 0;
-    char cstock = ' ';
+	FILE *fptr = NULL;
+	size_t count = 0;
+	char cstock = ' ';
 
-    fptr = fopen(filename, "r");
-    if (letters == 0 || filename == NULL || fptr == NULL)
-        {
-            return(count);
-        }
-   
-        while ((cstock = fgetc(fptr)) != EOF)
-        {
-            putchar(cstock);
-            count++;
-            if(count >= letters)
-            {
-                break;
-            }
-        }
-    fclose(fptr);
+	fptr = fopen(filename, "r");
+	if (letters == 0 || filename == NULL || fptr == NULL)
+	{
+		fclose(fptr);
+		return (count);
+	}
 
-    return (count);
+	while ((cstock = fgetc(fptr)) != EOF)
+	{
+		putchar(cstock);
+		count++;
+		if (count >= letters)
+		{
+			break;
+		}
+	}
+	fclose(fptr);
+
+	return (count);
 }
