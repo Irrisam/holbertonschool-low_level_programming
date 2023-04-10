7fd2b6cca182af7661cc0a288f9472b06dc8c3cc
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-10 20:03:19 +0200

    okay, extremely hard, altough betty is checked, we'll see each other again soon

---
+++ file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * main - copies content from one file to another
+ * @argc: number of input elements
+ * @argv: elements form stdin stored in arrays
+ * Return: 1 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int fptrsrc = 0;
+	int fptrdest = 0;
+	char buffer[1024];
+	ssize_t bytes_read;
+
+	(void)argc;
+	if (argv[3])
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		exit(97);
+	}
+	if (access(argv[1], R_OK) != 0)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fptrsrc = open(argv[1], O_RDONLY);
+	if (fptrsrc < 0)
+	{
+		printf("Error: Can't write to %s\n", argv[1]);
+		exit(99);
+	}
+	fptrdest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fptrdest < 0)
+	{
+		printf("Error: Can't open destination file %s\n", argv[2]);
+		exit(99);
+	}
+	while ((bytes_read = read(fptrsrc, buffer, 1024)) > 0)
+	{
+		if (write(fptrdest, buffer, bytes_read) != bytes_read)
+		{
+			printf("Error: Can't write to destination file %s\n", argv[2]);
+			exit(101);
+		}
+	}
+	close(fptrsrc);
+	close(fptrdest);
+	return (1);
+}
