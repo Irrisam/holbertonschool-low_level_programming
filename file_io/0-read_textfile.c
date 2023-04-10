96152857847b986f1c74b3aa862cb8d03c27a6f2
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-10 13:37:28 +0200

    fixed puts by putchar, seems to be working altough count might be off

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * 
+ * read_textfile - reads a txt file and prints output in POSIX
+ * @filename: name of the file to be read
+ * @letters: number of letters to be printed
+ * Return: count of printed chars
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -9,17 +12,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
     FILE * fptr;
     int count = 0;
     char * line;
+    int i = 0;
 
     fptr = fopen(filename, "r");
-
     line = malloc(sizeof(char) * letters);
-
     while (!feof(fptr))
     {
         fgets(line, letters, fptr); 
-        count += puts(line);
+        while(line[i] != '\0')
+        {
+            count += putchar(line[i]);
+            i++;
+        }
+        i = 0;
     }
-    free(line);
     fclose(fptr);
 
     return(count);
