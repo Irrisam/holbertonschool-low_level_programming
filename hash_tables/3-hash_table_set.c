34985a8e40476ea9562a1e6a27cb2e4ae28b169b
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-11 17:55:38 +0200

    okay okay

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,56 +1,45 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - adds a new node in hash table or places
- * it at start of chain
- * @ht: hash table to fill
- * @key: key for hash function
- * @value: value for hash function
- * Return: 1 on success, 0 on fail
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: hash table
+ * @key: current key
+ * @value: value associated with the key
+ * Return: 1 if succeed or 0 if not
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int stock;
-	hash_node_t *new = NULL;
+	unsigned long int index;
+	hash_node_t *temp, *new;
 
 	if (ht == NULL || key == NULL || value == NULL)
 	{
 		return (0);
 	}
-	stock = key_index((unsigned char *)key, ht->size);
-	if (ht->array[stock] == NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
 	{
-		new = malloc(sizeof(hash_node_t) * 1);
-		if (new == NULL)
-		{
-			return (0);
-		}
-		new->value = strdup(value);
-		new->key = strdup(key);
-		ht->array[stock] = new;
-	}
-	else
-	{
-		while (ht->array[stock] != NULL)
-		{
-			if (strcmp(ht->array[stock]->key, key) == 0)
-			{
-				ht->array[stock]->value = strdup(value);
-				return(1);
-			}
-			ht->array[stock] = ht->array[stock]->next;
-		}
-		new = malloc(sizeof(hash_node_t) * 1);
-		if (new == NULL)
-		{
-			return (0);
-		}
-		new->value = strdup(value);
-		new->key = strdup(key);
-		new->next = ht->array[stock];
-		ht->array[stock] = new;
 
+		if (strcmp(temp->key, key) == 0)
+		{
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
 	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+
+	if (ht->array[index] != NULL)
+	{
+		new->next = ht->array[index];
+	}
+	ht->array[index] = new;
 	return (1);
 }
