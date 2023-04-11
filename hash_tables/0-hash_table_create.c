bf98bb7274da3f5883bdef550382825e45066aeb
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-11 13:19:59 +0200

    fixed code by simplifying

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,16 +9,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *bite;
-	hash_node_t *mnode;
 
 	bite = malloc(sizeof(hash_table_t) * size);
-	bite->size = size;
-	mnode = malloc(sizeof(hash_node_t) * size);
-	if (mnode == NULL)
+	if (bite == NULL)
 	{
 		return (NULL);
 	}
-	bite->array = &mnode;
 
 	return (bite);
 }
