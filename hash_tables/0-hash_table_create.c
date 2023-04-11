71b00bce5e7e6460ed5446ae76995b74f5142559
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-11 13:24:51 +0200

    still working in local but differently

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,18 +3,26 @@
 /**
  * hash_table_create - creates an empty hash table
  * @size: size of the hashtable
- * Return: pointer to hashtable created
+ * Return: pointer to hashtable
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *bite;
+	hash_table_t *table;
+	hash_node_t *mnode;
 
-	bite = malloc(sizeof(hash_table_t) * size);
-	if (bite == NULL)
+	table = malloc(sizeof(int) * size);
+	if (table == NULL)
 	{
 		return (NULL);
 	}
+	table->size = size;
+	mnode = malloc(sizeof(int) * size);
+	if (mnode == NULL)
+	{
+		return (NULL);
+	}
+	table->array = &mnode;
 
-	return (bite);
+	return (table);
 }
