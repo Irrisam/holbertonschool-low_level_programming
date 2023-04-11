91cfff0d9cd3700132c03caa2cebdfc018575c4d
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-11 14:14:05 +0200

    works in localgit add .git add . updated

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,12 +9,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	
-	table = malloc(sizeof(unsigned long int) * size + 16);
-	
+
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
+
+	table->size = size;
+
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+
+
 	return (table);
 }
