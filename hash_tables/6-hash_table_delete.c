3919bef94e99c811af8a5f392a6453d9e93e1e17
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-04-12 16:01:59 +0200

    great commit, added, fixed, replaced, considered, null, working, later, done

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,47 +1,49 @@
 #include "hash_tables.h"
 
 /**
- * 
-*/
+ * hash_table_delete - deletes hash table
+ * @ht: hash table to work in
+ */
 
 void hash_table_delete(hash_table_t *ht)
 {
-    
-    unsigned long int counter = 0;
-    hash_node_t *tmp = NULL;
-    hash_node_t *runner = NULL;
+
+	unsigned long int counter = 0;
+	hash_node_t *tmp = NULL;
+	hash_node_t *runner = NULL;
 
 	if (ht == NULL)
 	{
 		return;
 	}
-    if (ht->array == NULL)
-    {
-        free(ht->array);
-        free(ht);
-    }
-    runner = ht->array[counter];
-    tmp = ht->array[counter];
-
-	while (counter < ht->size)
+	if (ht->array == NULL)
 	{
-        if (ht->array[counter])
-        {
-            tmp = ht->array[counter];
+		free(ht->array);
+		free(ht);
+		return;
+	}
+	runner = ht->array[counter];
+	tmp = ht->array[counter];
+
+	while (counter <= ht->size)
+	{
+		if (ht->array[counter])
+		{
+			tmp = ht->array[counter];
 			while (tmp)
 			{
-                 runner = tmp->next;
-                 free(tmp->value);
-                 tmp->value = NULL;
-                 free(tmp->key);
-                 tmp->key = NULL;
-                 free(tmp);
-                 tmp = runner;
-                 
+				runner = tmp->next;
+				free(tmp->value);
+				tmp->value = NULL;
+				free(tmp->key);
+				tmp->key = NULL;
+				free(tmp);
+				tmp = runner;
+
 			}
-        }
-        counter++;
+		}
+		counter++;
 	}
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
