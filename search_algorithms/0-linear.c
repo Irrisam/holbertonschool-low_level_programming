6d128b96d67f85a6daeb5ab95e1a873789aef3f7
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-05-24 02:49:04 -0700

    task 0 tested and bettied

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * linear_search - searches for a value in a linear array
+ * @size: size of array
+ * @value : size to search for
+ * @array: pointer to given array to search in
+ * Return: index of found value on success, -1 on failure
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	for (; i < size - 1; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
